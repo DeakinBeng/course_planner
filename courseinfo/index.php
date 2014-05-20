6aa57eb6a5a336b313ac9e6a162f411e20094d00
DeakinBeng <blim@deakin.edu.au>
2014-05-20 15:12:44 +1000

    Use lowercase for file names

--- courseinfo/index.php
+++ courseinfo/index.php
@@ -1,13 +1,13 @@
 <script> 
 function newWindow() { 
-window.open("CInfoViewer.php/?code="+document.getElementsByName('code')[0].value+"&cu="+(document.getElementsByName('cu')[0].checked ? "course" : "unit"), "", "menubar=0, resizable=0,dependent=0,status=0,width=660,height=700,left=10,top=10"); 
+window.open("cinfoviewer.php/?code="+document.getElementsByName('code')[0].value+"&cu="+(document.getElementsByName('cu')[0].checked ? "course" : "unit"), "", "menubar=0, resizable=0,dependent=0,status=0,width=660,height=700,left=10,top=10"); 
 
 return false;
 } 
 </script>
 <?php
 
-echo "<form action='CInfoViewer.php' method='GET' onSubmit='return newWindow()' >
+echo "<form action='cinfoviewer.php' method='GET' onSubmit='return newWindow()' >
 	Code: <input type='text' name='code' autofocus autocomplete='off' />
 	<br />
 	<input type='radio' name='cu' value='course' checked>Course &nbsp;
--- sqlmaker/index.php
+++ sqlmaker/index.php
@@ -1,13 +1,13 @@
 <script> 
 function newWindow() { 
-window.open("SQL.php/?code="+document.getElementsByName('code')[0].value+"&cu="+(document.getElementsByName('cu')[0].checked ? "course" : "unit"), "", "menubar=0, resizable=0,dependent=0,status=0,width=660,height=700,left=10,top=10"); 
+window.open("sql.php/?code="+document.getElementsByName('code')[0].value+"&cu="+(document.getElementsByName('cu')[0].checked ? "course" : "unit"), "", "menubar=0, resizable=0,dependent=0,status=0,width=660,height=700,left=10,top=10"); 
 
 return false;
 } 
 </script>
 <?php
 
-echo "<form action='SQL.php' method='GET' onSubmit='return newWindow()' >
+echo "<form action='sql.php' method='GET' onSubmit='return newWindow()' >
 	Code: <input type='text' name='code' autofocus autocomplete='off' />
 	<br />
 	<input type='radio' name='cu' value='course' checked>Course &nbsp;
