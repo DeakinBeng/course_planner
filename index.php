b5c4ab03c229253439a8ae33e558574c54145385
jigenshrestha <jigenshrestha@gmail.com>
2014-05-24 17:07:10 +1000

    header and footer separated 2.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	include_once("header.php">
+	include_once("header.php");
 ?>
 
 <div class="wrapper">
@@ -38,5 +38,5 @@
 </div>
 
 <?php
-	include_once("footer.php">
+	include_once("footer.php");
 ?>
