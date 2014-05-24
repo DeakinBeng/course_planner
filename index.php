e4e292d2834e5f0fb535068ca55e4abcce27c05b
jigenshrestha <jigenshrestha@gmail.com>
2014-05-24 17:05:32 +1000

    header and footer file separated.

--- index.php
+++ index.php
@@ -1,40 +1,7 @@
 <?php
-	include_once("database.php");
+	include_once("header.php">
 ?>
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Course Planner</title>
 
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script type="text/javascript">
-
-		$(function() {
-
-			  $('#courseSelection').change(function(){
-				//alert("load-course-major.php?course_code=" + $('#courseSelection').val());
-				load_course_major('load-course-major.php?course_code=' + $('#courseSelection').val());
-			});
-
-		});
-		/*function to load a list*/
-		function load_course_major(url)
-		{
-			$("#majorSelection").empty();
-			$.getJSON(url,null,function(data)
-			{
-				$.each(data, function(i,obj)
-				{
-					$("#majorSelection").append($('<option></option>').val(obj['Major_ID']).html(obj['Major_Title']));
-				});
-			});
-		}
-	</script>
-</head>
-
-<body>
 <div class="wrapper">
 	<form method="post" id="frmSearch" name="frmSearch" action="course-template.php">
     	<h1>Campus Or online Selection</h1>
@@ -69,5 +36,7 @@
         <button type="submit" id="btnSubmit" name="btnSubmit" />Generate Plan</button>
     </form>
 </div>
-</body>
-</html>
+
+<?php
+	include_once("footer.php">
+?>
